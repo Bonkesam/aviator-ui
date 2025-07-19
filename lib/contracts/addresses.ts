1c648ca5eafb995bfe4ee2a72f239cb23f13ab1c
Bonkesam <bonkesam9@gmail.com>
2025-07-20 02:37:02 +0300

    adds addresses

---
+++ lib/contracts/addresses.ts
@@ -0,0 +1,5 @@
+// lib/contracts/addresses.ts
+export const CONTRACT_ADDRESSES = {
+  AVIATOR: "0xDDb3F89a48d3F18683275009A45755627c3fE6bF" as const,
+  VRF_CONSUMER: "0x00620972f05dCc747Fc9935d72499CBe4D83AfB1" as const,
+} as const;
